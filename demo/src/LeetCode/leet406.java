18d2db70c44c31c5fa86cf7843b18fd087e7e834
ming <1308359434@qq.com>
2019-11-21 11:31:27 +0800

    leetcode

---
+++ demo/src/LeetCode/leet406.java
@@ -0,0 +1,34 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author m
+ * @version 1.0
+ * @date 2019/11/20 15:24
+ */
+public class leet406 {
+    public int[][] reconstructQueue(int[][] people) {
+        // [7,0], [7,1], [6,1], [5,0], [5,2], [4,4]
+        // 再一个一个插入。
+        // [7,0]
+        // [7,0], [7,1]
+        // [7,0], [6,1], [7,1]
+        // [5,0], [7,0], [6,1], [7,1]
+        // [5,0], [7,0], [5,2], [6,1], [7,1]
+        // [5,0], [7,0], [5,2], [6,1], [4,4], [7,1]
+        if (people == null || people.length == 0 || people[0].length == 0) {
+            return new int[0][0];
+        }
+        Arrays.sort(people, (a, b) -> (a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]));
+        List<int[]> queue = new ArrayList<>();
+        for (int[] p : people) {
+            queue.add(p[1], p);
+        }
+        return queue.toArray(new int[queue.size()][]);
+    }
+
+}
