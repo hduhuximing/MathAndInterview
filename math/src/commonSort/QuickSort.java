f4bcaaba7ccf2358d463fa9e8cd7cd6ca19e554f
ming <huximing.hdu@bytedance.com>
2021-07-08 12:35:56 +0800

    移除pom
    Change-Id: If0f0ef5ff62f6948640601c823d8e5f36217ff0b

--- math/src/commonSort/QuickSort.java
+++ math/src/commonSort/QuickSort.java
@@ -19,26 +19,26 @@ public class QuickSort {
         if (start < end) {
             //把数组中的第0个数字做为标准数
 //            swap(arr, start, (int) Math.random() * (end - start + 1)+start );
-            int stard = arr[start];
+            int index = arr[start];
             //记录需要排序的下标
             int low = start;
             int high = end;
             //循环找比标准数大的数和比标准数小的数
             while (low < high) {//相等跳出
                 //右边的数字比标准数大
-                while (low < high && stard <= arr[high]) {
+                while (low < high && index <= arr[high]) {
                     high--;
                 }
                 //使用右边的数字替换左边的数
                 arr[low] = arr[high];
                 //如果左边的数字比标准数小
-                while (low < high && arr[low] <= stard) {
+                while (low < high && arr[low] <= index) {
                     low++;
                 }
                 arr[high] = arr[low];
             }
             //把标准数赋给低所在的位置的元素
-            arr[low] = stard;
+            arr[low] = index;
             //low已经被排序完了
             //处理所有的小的数字
             quickSort(arr, start, low - 1);
